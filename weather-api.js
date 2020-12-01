e6c9a253b611ee2ad76e689630f2a175cf1fe7bb
liyaSileshi <liyasileshi@gmail.com>
2020-11-30 22:04:20 -0800

    switched to async await

--- weather-api.js
+++ weather-api.js
@@ -1,14 +1,8 @@
 
-function getWeather(zip, apiKey, success, error) {
+async function getWeather(zip, apiKey) {
   const units = 'imperial'
   const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
-  fetch(path)
-    .then(res => res.json())
-    .then(json => {
-      success(json)
-    })
-    .catch(err => 
-      error(err))
+  const res = await fetch(path)
+  const json = await res.json()
+  return json //returns the whole promise
 }
-
-
