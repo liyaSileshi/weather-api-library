94f4caf8c53dec9e355a07d446a62f0d37ffdf3d
liyaSileshi <liyasileshi@gmail.com>
2020-12-03 20:32:39 -0800

    added more methods

--- weather-api.js
+++ weather-api.js
@@ -4,12 +4,65 @@ class OpenWeather {
     this.units = units
   }
 
-  async getWeather(zip) {
+  async getWeatherByZip(zip) {
     const {apiKey, units} = this
     const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
     const res = await fetch(path)
     const json = await res.json()
-    return json //returns the whole promise
+    // return json //returns the whole promise
+
+    // Get all of the relavant info 
+    const { base, clouds, cod, coord, dt, id, main, name, sys, timezone, visibility, weather, wind } = json
+    const { temp, pressure, humidity, temp_max, temp_min } = main
+    const { description, icon } = weather[0]
+    // Reformat the object that is returned
+    return { temp, pressure, humidity, temp_min, temp_max, clouds, cod, visibility, wind, description, icon }
+
+  }
+
+  async getWeatherByCity(city) {
+    const {apiKey, units} = this
+    const path = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`
+    const res = await fetch(path)
+    const json = await res.json()
+    // return json //returns the whole promise
+
+    // Get all of the relavant info 
+    const { base, clouds, cod, coord, dt, id, main, name, sys, timezone, visibility, weather, wind } = json
+    const { temp, pressure, humidity, temp_max, temp_min } = main
+    const { description, icon } = weather[0]
+    // Reformat the object that is returned
+    return { temp, pressure, humidity, temp_min, temp_max, clouds, cod, visibility, wind, description, icon }
+  }
+
+  async getWeatherByCityID(cityId) {
+    const {apiKey, units} = this
+    const path = `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${apiKey}&units=${units}`
+    const res = await fetch(path)
+    const json = await res.json()
+    // return json //returns the whole promise
+
+    // Get all of the relavant info 
+    const { base, clouds, cod, coord, dt, id, main, name, sys, timezone, visibility, weather, wind } = json
+    const { temp, pressure, humidity, temp_max, temp_min } = main
+    const { description, icon } = weather[0]
+    // Reformat the object that is returned
+    return { temp, pressure, humidity, temp_min, temp_max, clouds, cod, visibility, wind, description, icon }
+  }
+
+  async getWeatherByGeo(lat, lon) {
+    const {apiKey, units} = this
+    const path = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`
+    const res = await fetch(path)
+    const json = await res.json()
+    // return json //returns the whole promise
+
+    // Get all of the relavant info 
+    const { base, clouds, cod, coord, dt, id, main, name, sys, timezone, visibility, weather, wind } = json
+    const { temp, pressure, humidity, temp_max, temp_min } = main
+    const { description, icon } = weather[0]
+    // Reformat the object that is returned
+    return { temp, pressure, humidity, temp_min, temp_max, clouds, cod, visibility, wind, description, icon }
   }
 }
 
